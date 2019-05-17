83526e37b306775c4b9730e4f6e2357f006b8a18
benclark1110 <benclark1110@gmail.com>
2019-05-17 00:02:34 -0400

    Customer aspect nearly completed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,49 @@
+var mysql = require("mysql");
+
+var cTable = require('console.table');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
 var inquirer = require('inquirer');
 
-inquirer
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  showAllItems();
+});
+
+function showAllItems() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+
+      // console.table([
+      //   {
+      //     productName: res[i].product_name,
+      //     Department: res[i].department_name,
+      //     Price: res[i].price,
+      //     Quantity: res[i].stock_quantity
+      //   }
+      // ]);
+    }
+    askConsumer();
+  });
+}
+
+function askConsumer() {
+  inquirer
   .prompt([
     {
         type: "input",
@@ -23,12 +66,38 @@ inquirer
   .then(function(answers) {
     
     if (answers.howMany == 0) {
-        console.log("Maybe next time...")
-    } else if (answers.howMany == 1) {
-    console.log("Cool, you bought " + answers.howMany + " " + requestedID);
-    } else {
-    console.log("Cool, you bought " + answers.howMany + " " + requestedID + "s");
-    }
+      console.log("Maybe next time...");
+      endSimulation();
+    } 
+
+    var query = "SELECT * FROM products WHERE ?"
+    connection.query(query, { product_name: requestedID }, function(err, res) {
+
+      howManyItems = parseInt(answers.howMany);
+      stockQuantity = parseInt(res[0].stock_quantity);
+
+      if (res.length == 0){
+        console.log("Item does not exist, please try again");
+        endSimulation();
+      };
+
+      if (howManyItems > stockQuantity) {
+        console.log("Insufficient quantity!")
+        endSimulation();
+      } else {
+        console.log("Total order cost: $" + (howManyItems * res[0].price));
+
+        /////////////////////////////////////////fix below query
+        connection.query("UPDATE products SET stock_quantity = " + (stockQuantity - howManyItems) + " WHERE product_name = " + requestedID, function(err, res) {
+          endSimulation();
+        });
+      }
+    });
   });
- 
   });
+};
+
+function endSimulation() {
+  connection.end();
+  return;
+};
